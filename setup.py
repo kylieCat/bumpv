fae4ff615658986886ccf70648566ba983f61f49
kylie auld <kylie@email.com>
2024-03-04 12:39:29 -0800

    update bumpv stuff

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = re.sub(
 
 setup(
     name='bumpv',
-    version='0.5.0',
+    version='0.5.3',
     url='https://github.com/kylie-a/bumpv',
     author='Kylie Auld',
     author_email='kylie.a@protonmail.com',
