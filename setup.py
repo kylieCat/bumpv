db8f0de9041e60233c8b062a423922d7397cf78b
Matt Schiros <schiros@gmail.com>
2025-01-20 14:49:00 -0800

    Bump version: 0.6.3-schiros+abc → 0.6.4-schiros+abc

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = re.sub(
 
 setup(
     name='bumpv',
-    version='0.6.3-schiros+abc',
+    version='0.6.4-schiros+abc',
     url='https://github.com/kylie-a/bumpv',
     author='Kylie Auld',
     author_email='kylie.a@protonmail.com',
