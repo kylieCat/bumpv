31a80a3933c0e4a3b6e3b5f048743f985e47cfb2
Matt Schiros <schiros@gmail.com>
2025-01-21 11:36:30 -0800

    Bump version: 0.6.9-schiros+abc → 0.6.10-schiros+abc

--- bumpv/__init__.py
+++ bumpv/__init__.py
@@ -5,7 +5,7 @@ from .client import BumpClient, exceptions
 from .client.config import Configuration
 from .client.versioning import Version
 
-__VERSION__ = '0.6.9-schiros+abc'
+__VERSION__ = '0.6.10-schiros+abc'
 
 py_ver = sys.version.split('\n')[0].split(' ')[0]
 DESCRIPTION = f"bumpv: v{__VERSION__} (using Python v{py_ver})"
